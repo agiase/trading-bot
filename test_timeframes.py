1518674af8061f0f2920d8ab43777a64446d18d9
Agiase <agiase@agiase.ai>
2026-05-19 13:25:31 +0000

    remove test files

--- test_timeframes.py
+++
@@ -1,9 +0,0 @@
-import sys; sys.path.insert(0, '.')
-from exchange.hyperliquid_executor import HyperliquidExecutor
-ex = HyperliquidExecutor()
-for tf in ['15s', '30s', '1m', '3m', '5m', '15m']:
-    candles = ex.fetch_ohlcv('BTC', timeframe=tf, limit=3)
-    if isinstance(candles, list) and len(candles) > 0:
-        print(f'{tf}: last close={candles[-1]["close"]}, vol={candles[-1]["volume"]:.2f}')
-    else:
-        print(f'{tf}: {candles}')
